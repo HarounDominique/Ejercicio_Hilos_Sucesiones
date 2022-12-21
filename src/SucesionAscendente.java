ff0d2f6e01e7b6c6574f78d24b63402231701191
HarounDominique <haroundominique@gmail.com>
2022-12-21 21:37:48 +0100

    Initial commit

---
+++ src/SucesionAscendente.java
@@ -0,0 +1,29 @@
+public class SucesionAscendente extends Thread {
+    int inicio;
+    int fin;
+    String nombre;
+
+    public SucesionAscendente(int inicio, int fin, String nombre) {
+        super(nombre);
+        this.inicio = inicio;
+        this.fin = fin;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " está ejecutándose.");
+        for (int i = inicio; i < fin; i++) {
+            System.out.println(Thread.currentThread().getName() + ":" + i);
+        }
+        System.out.println(Thread.currentThread().getName() + " ha finalizado.");
+    }
+
+    public static void main(String[] args) {
+        SucesionAscendente h1 = new SucesionAscendente(5, 15, "HiloA");
+        SucesionAscendente h2 = new SucesionAscendente(7, 20, "HiloB");
+
+        h1.start();
+        h2.start();
+    }
+}
